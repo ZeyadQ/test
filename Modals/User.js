eeb92e0b5eb261a69e27cc42e79ea626aeb08ccf
Zeyad-Qatash <Zeyad.Qatash@ibm.com>
2019-10-28 15:10:33 -0500

    recent update10-28-2019

---
+++ Modals/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required :true
+    },
+    email: {
+        type: String,
+        required : true,
+        unique:true
+    },
+    
+    password:  {
+        type: String,
+        required : true,
+    },
+
+    avatar : {
+        type: String,
+        required : true,
+        unique:true
+    },
+
+     date: {
+        type: Date,
+        defualt :Date.now
+    }
+
+});
+module.exports= User = mongoose.model('user',UserSchema);
--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express'); // get express server up and running 
+const connectDB = require('./config/db');
+
 const app = express(); // initialize our app variable with express
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({extended: false}));
+
 app.get ('/', (req,res) => res.send('API Running '));
+
+// define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;// we put the port in avariable 
-app.listen(PORT,()=> console.log(`Server started omn port ${PORT} `)); // listen on a port 
+
+app.listen(PORT,() => console.log(`Server started omn port ${PORT} `)); // listen on a port 
